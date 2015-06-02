ed558cbd7417592c87f270607f16d5a10526c016
Douglas Yan <douglasyan@Douglass-MacBook-Pro.local>
2015-06-02 16:39:13 -0700

    Working in JavaScript.

--- calc.js
+++ calc.js
@@ -1,11 +1,9 @@
-var size = process.argv.length;
 var functions = require('./functions.js');
 
-//All one string
-if(size > 3){
+if(process.argv.length !== 2){
 	functions.readString(process.argv);
 }
 
-else if(size === 3){
-	functions.parseString(process.argv[2]);
+else{
+	console.log('Empty input');
 }
--- functions.js
+++ functions.js
@@ -1,17 +1,15 @@
-function parseString(dataString){
-	console.log(eval(dataString));
-}
-
 function readString(dataString){
 	var size = dataString.length;
 	var evalString = '';
 
 	for(var i = 2; i < size; i++){
+		//'*' character in bash has a special meaning, so need to check for that
 		if(dataString[i] === 'calc.js' || dataString[i] === 'functions.js'){
 			evalString += '*';
 			i++;
 		}
 
+		//move everything into one string
 		else if(dataString[i] != ' '){
 			evalString += dataString[i];
 		}
@@ -20,5 +18,4 @@ function readString(dataString){
 	console.log(eval(evalString));
 }
 
-module.exports.parseString = parseString;
 module.exports.readString = readString;
