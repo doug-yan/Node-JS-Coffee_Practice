48a7be11a4622f0db67980073fe0d095f55066a6
Douglas Yan <douglasyan@Douglass-MacBook-Pro.local>
2015-06-02 15:46:19 -0700

    Used eval() instead of parsing manually.

--- calc.js
+++ calc.js
@@ -1,11 +1,10 @@
 var size = process.argv.length;
 var functions = require('./functions.js');
 
-//Check to make sure that the first item is a number
 if(size === 3){
 	functions.parseString(process.argv[2]);
 }
 
-else{
+else if(size > 3){
 	functions.readString(process.argv);
 }
