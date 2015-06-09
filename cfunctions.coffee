38efb624094e51f308f08bb5fe02fb67a3f16349
Douglas Yan <douglasyan@Douglass-MacBook-Pro.local>
2015-06-09 16:24:22 -0700

    Get everything up-to-date.

--- cfunctions.coffee
+++ cfunctions.coffee
@@ -1,6 +1,13 @@
 parse = (data) ->
 	evalString = ''
-	evalString += item for item in data[2...] when item isnt ' ' 
+	for item in data[2...] when item isnt ' ' 
+		if item is 'calc.js'# and item+1 is 'cfunctions.coffee' and item+2 is 'coffeeCalc.coffee' or item+3 is 'functions.js'
+			console.log 'found'
+			evalString += '*'
+			item
+		console.log evalString
+		evalString += item
+
 	console.log eval evalString
 
 module.exports.parse = parse
--- functions.js
+++ functions.js
@@ -4,9 +4,19 @@ function readString(dataString){
 
 	for(var i = 2; i < size; i++){
 		//'*' character in bash has a special meaning, so need to check for that
-		if(dataString[i] === 'calc.js' || dataString[i] === 'functions.js'){
+		if(dataString[i] === 'calc.js' && dataString[i+1] === 'cfunctions.coffee' && dataString[i+2] === 'coffeeCalc.coffee' && dataString[i+3] === 'functions.js'){
 			evalString += '*';
+			i+=3;
+		}
+
+		//take care of parentheses
+		else if(dataString[i] === '(') {
+			var tempString = '';
 			i++;
+			while(dataString[i] !== ')'){
+				tempString += dataString[i];
+			}
+			evalString += eval(tempString);
 		}
 
 		//move everything into one string
